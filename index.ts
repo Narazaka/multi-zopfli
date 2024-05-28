17d20350ba0e162b2917ed86ea9d7484b78e4f1d
Narazaka <info@narazaka.net>
2024-05-28 19:46:24 +0900

    options

--- index.ts
+++ index.ts
@@ -18,8 +18,40 @@ const cmd = command({
     args: {
         glob: positional({type: string, displayName: "glob", description: "Glob pattern to match files"}),
         concurrency: option({type: number, long: "concurrency", short: "c", description: "Number of parallel processes to run", defaultValue: () => Math.ceil(os.cpus().length / 2)}),
+        m: flag({long: "m", short: "m", description: "compress more: use more iterations (depending on file size)"}),
+        lossy_transparent: flag({long: "lossy_transparent", description: "remove colors behind alpha channel 0. No visual difference, removes hidden information."}),
+        lossy_8bit: flag({long: "lossy_8bit", description: "convert 16-bit per channel image to 8-bit per channel."}),
+        q: flag({long: "q", short: "q", description: "use quick, but not very good, compression (e.g. for only trying the PNG filter and color types)"}),
+        iterations: option({type: number, long: "iterations", defaultValue: () => -1, description: "number of iterations, more iterations makes it slower but provides slightly better compression. Default: 15 for small files, 5 for large files."}),
+        filters: option({
+            type: string, long: "filters", defaultValue: () => "",
+            description:
+                "filter strategies to try:"
+                + "\n   0-4: give all scanlines PNG filter type 0-4"
+                + "\n   m: minimum sum"
+                + "\n   e: entropy"
+                + "\n   p: predefined (keep from input, this likely overlaps another strategy)"
+                + "\n   b: brute force (experimental)"
+                + "\n   By default, if this argument is not given, one that is most likely the best for this image is chosen by trying faster compression with each type."
+                + "\n   If this argument is used, all given filter types are tried with slow compression and the best result retained. A good set of filters to try is --filters=0me."
+        }),
+        keepchunks: option({
+            long: "keepchunks", type: string, defaultValue: () => "",
+            description:
+                "keep metadata chunks with these names that would normally be removed, e.g. tEXt,zTXt,iTXt,gAMA, ..."
+                + "\n   Due to adding extra data, this increases the result size. Keeping bKGD or sBIT chunks may cause additional worse compression due to forcing a certain color type, it is advised to not keep these for web images because web browsers do not use these chunks. By default ZopfliPNG only keeps (and losslessly modifies) the following chunks because they are essential: IHDR, PLTE, tRNS, IDAT and IEND."
+        }),
     },
     handler: async (args) => {
+        const options: string[] = [];
+        if (args.m) options.push("-m");
+        if (args.lossy_transparent) options.push("--lossy_transparent");
+        if (args.lossy_8bit) options.push("--lossy_8bit");
+        if (args.q) options.push("-q");
+        if (args.iterations !== -1) options.push(`--iterations=${args.iterations}`);
+        if (args.filters) options.push(`--filters=${args.filters}`);
+        if (args.keepchunks) options.push(`--keepchunks=${args.keepchunks}`);
+
         const queue = new PQueue({concurrency: args.concurrency, autoStart: false});
         const sizes: {[input: string]: {beforeSize: number; afterSize?: number}} = {};
         for await (const entry of globbyStream(slash(args.glob), {onlyFiles: true})) {
@@ -29,8 +61,10 @@ const cmd = command({
             sizes[input] = {beforeSize};
             queue.add(() =>
                 new Promise<void>((resolve, reject) => {
+                    const allOptions = options.concat(['-y', input, input]);
+                    console.warn([">", path.basename(zopflipng)].concat(allOptions).join(" "));
                     console.warn(`Optimizing [${filesize(beforeSize).human()}] ${input}`);
-                    execFile(zopflipng, ['-y', '-m', '--lossy_transparent', input, input], (err) => {
+                    execFile(zopflipng, allOptions, (err) => {
                         if (err) return reject(err);
                         const afterSize = fs.statSync(input).size;
                         sizes[input].afterSize = afterSize;
